c9e33c6bfa6ee558aef5b4f88f50545b5f62e95a
AdvenAdam <advenadam@student.uns.ac.id>
2025-02-11 14:10:16 +0700

    update migration file

--- database/migrations/2024_10_05_151003_create_generus_table.php
+++ database/migrations/2024_10_05_151003_create_generus_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::disableForeignKeyConstraints();
 
-        Schema::create('jamaah', function (Blueprint $table) {
+        Schema::create('generus', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->date('tgllahir');
@@ -44,6 +44,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jamaah');
+        Schema::dropIfExists('generus');
     }
 };
