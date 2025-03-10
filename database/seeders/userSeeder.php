8ac670527b1845eb34f1fc06390277a67d784de1
AdvenAdam <advenadam@student.uns.ac.id>
2025-03-10 09:40:32 +0700

    update seeder

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -2,10 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Desa;
+use App\Models\Kelompok;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class userSeeder extends Seeder
 {
@@ -14,6 +17,40 @@ class userSeeder extends Seeder
      */
     public function run(): void
     {
+        $kelompok = Kelompok::all();
+        foreach ($kelompok as $k) {
+            DB::table('users')->insert([
+                [
+                    'nama' => $k->nama,
+                    'email' => Str::slug($k->nama) . '-kel' . '@boybar.com',
+                    'password' => Hash::make('password123'), // Pastikan untuk mengenkripsi password
+                    'id_daerah' => 1, // Sesuaikan dengan data yang ada
+                    'id_desa' => $k->id_desa,   // Sesuaikan dengan data yang ada
+                    'id_kelompok' => $k->id, // Sesuaikan dengan data yang ada
+                    'jabatan' => 'kelompok',
+                    'foto' => 'logo.png',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+            ]);
+        }
+        $desa = Desa::all();
+        foreach ($desa as $d) {
+            DB::table('users')->insert([
+                [
+                    'nama' => $d->nama,
+                    'email' => Str::slug($d->nama) . '-desa' . '@boybar.com',
+                    'password' => Hash::make('password123'), // Pastikan untuk mengenkripsi password
+                    'id_daerah' => 1, // Sesuaikan dengan data yang ada
+                    'id_desa' => $d->id,   // Sesuaikan dengan data yang ada
+                    'id_kelompok' => 1, // Sesuaikan dengan data yang ada
+                    'jabatan' => 'desa',
+                    'foto' => 'logo.png',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+            ]);
+        }
         DB::table('users')->insert([
             [
                 'nama' => 'Administrator',
@@ -22,7 +59,7 @@ class userSeeder extends Seeder
                 'id_daerah' => 1, // Sesuaikan dengan data yang ada
                 'id_desa' => 1,   // Sesuaikan dengan data yang ada
                 'id_kelompok' => 1, // Sesuaikan dengan data yang ada
-                'jabatan' => 'kelompok',
+                'jabatan' => 'daerah',
                 'foto' => 'logo.png',
                 'created_at' => now(),
                 'updated_at' => now(),
